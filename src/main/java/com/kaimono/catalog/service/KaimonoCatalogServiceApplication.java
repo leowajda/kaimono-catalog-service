9f71cfefbfe1310a58f913b7556891aa4512f937
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-25 20:28:27 +0100

    :tada: Start kaimono-catalog-service

---
+++ src/main/java/com/kaimono/catalog/service/KaimonoCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaimono.catalog.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaimonoCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaimonoCatalogServiceApplication.class, args);
+	}
+
+}
