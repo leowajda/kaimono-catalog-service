b9d26ce682553b39a1f7c4fa7bb1f7851dee5f27
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-26 16:27:26 +0100

    :sparkles: Add domain objects

---
+++ src/main/java/com/kaimono/catalog/service/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.kaimono.catalog.service.domain;
+
+public class BookNotFoundException extends  RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
---
+++ src/main/java/com/kaimono/catalog/service/domain/BookRepository.java
@@ -0,0 +1,14 @@
+package com.kaimono.catalog.service.domain;
+
+import java.util.Optional;
+
+public interface BookRepository {
+
+    Iterable<Book> findAll();
+    Optional<Book> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
+    void deleteByIsbn(String isbn);
+    Book save(Book book);
+
+
+}
