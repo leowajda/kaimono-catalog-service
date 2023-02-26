be93d06a2d200bac8613aa40b411b30101d6c766
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-26 16:28:20 +0100

    :sparkles: Add RESTful service with in-memory database

---
+++ src/main/java/com/kaimono/catalog/service/web/BookControllerAdvice.java
@@ -0,0 +1,37 @@
+package com.kaimono.catalog.service.web;
+
+import com.kaimono.catalog.service.domain.BookAlreadyExistsException;
+import com.kaimono.catalog.service.domain.BookNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class BookControllerAdvice {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String bookNotFoundHandler(BookNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(BookAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public String bookAlreadyExistsHandler(BookAlreadyExistsException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getAllErrors().stream().map(error -> Map.entry(((FieldError) error).getField(), error.getDefaultMessage()))
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+}
